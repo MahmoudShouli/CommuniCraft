3e7bd27f0553374487a3c2f8c42988d32a93be18
NabeelJamous <xnabeel972@gmail.com>
2024-02-27 21:38:35 +0200

    init. project

---
+++ src/test/java/com/aswe/communicraft/CommunicraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aswe.communicraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
